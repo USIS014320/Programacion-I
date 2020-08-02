5108b178093466a2ad6af69c5acc0691eb17b10c
Denys Alexander Hernandez Maravilla <usis016320@ugb.edu.sv>
2020-08-02 16:21:57 -0600

    Tablas de multiplicar menores a 12

--- holaMundo/Form1.Designer.vb
+++ holaMundo/Form1.Designer.vb
@@ -22,43 +22,54 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.ListBox1 = New System.Windows.Forms.ListBox()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.LstTablas = New System.Windows.Forms.ListBox()
+        Me.btnProcesar = New System.Windows.Forms.Button()
+        Me.txtntabla = New System.Windows.Forms.TextBox()
+        Me.lbltabla = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
-        'ListBox1
+        'LstTablas
         '
-        Me.ListBox1.FormattingEnabled = True
-        Me.ListBox1.Location = New System.Drawing.Point(273, 12)
-        Me.ListBox1.Name = "ListBox1"
-        Me.ListBox1.Size = New System.Drawing.Size(121, 173)
-        Me.ListBox1.TabIndex = 0
+        Me.LstTablas.FormattingEnabled = True
+        Me.LstTablas.Location = New System.Drawing.Point(250, 12)
+        Me.LstTablas.Name = "LstTablas"
+        Me.LstTablas.Size = New System.Drawing.Size(256, 173)
+        Me.LstTablas.TabIndex = 0
         '
-        'Button1
+        'btnProcesar
         '
-        Me.Button1.Location = New System.Drawing.Point(128, 170)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(109, 50)
-        Me.Button1.TabIndex = 1
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btnProcesar.Location = New System.Drawing.Point(97, 135)
+        Me.btnProcesar.Name = "btnProcesar"
+        Me.btnProcesar.Size = New System.Drawing.Size(109, 50)
+        Me.btnProcesar.TabIndex = 1
+        Me.btnProcesar.Text = "Generar tabla"
+        Me.btnProcesar.UseVisualStyleBackColor = True
         '
-        'TextBox1
+        'txtntabla
         '
-        Me.TextBox1.Location = New System.Drawing.Point(120, 38)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(96, 20)
-        Me.TextBox1.TabIndex = 2
+        Me.txtntabla.Location = New System.Drawing.Point(110, 31)
+        Me.txtntabla.Name = "txtntabla"
+        Me.txtntabla.Size = New System.Drawing.Size(96, 20)
+        Me.txtntabla.TabIndex = 2
+        '
+        'lbltabla
+        '
+        Me.lbltabla.AutoSize = True
+        Me.lbltabla.Location = New System.Drawing.Point(67, 31)
+        Me.lbltabla.Name = "lbltabla"
+        Me.lbltabla.Size = New System.Drawing.Size(37, 13)
+        Me.lbltabla.TabIndex = 3
+        Me.lbltabla.Text = "Tabla:"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(464, 262)
-        Me.Controls.Add(Me.TextBox1)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.ListBox1)
+        Me.ClientSize = New System.Drawing.Size(540, 259)
+        Me.Controls.Add(Me.lbltabla)
+        Me.Controls.Add(Me.txtntabla)
+        Me.Controls.Add(Me.btnProcesar)
+        Me.Controls.Add(Me.LstTablas)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
         Me.MaximizeBox = False
         Me.MinimizeBox = False
@@ -69,7 +80,8 @@ Partial Class Form1
 
     End Sub
 
-    Friend WithEvents ListBox1 As ListBox
-    Friend WithEvents Button1 As Button
-    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents LstTablas As ListBox
+    Friend WithEvents btnProcesar As Button
+    Friend WithEvents txtntabla As TextBox
+    Friend WithEvents lbltabla As Label
 End Class
--- holaMundo/Form1.vb
+++ holaMundo/Form1.vb
@@ -1,42 +1,18 @@
 ﻿Public Class Form1
     Private Sub btnCalcular_Click(sender As Object, e As EventArgs)
-        'Dim es una palabra reservada para declarar una variable
-        Dim num1, num2, respuesta As Double 'Declaro las variables en linea separadas por comas del mismo tipo
 
-        num1 = txtnum1.Text 'capturando el dato que ingres el usuario a la caja de texto, asigno dicho valor a la variable num1.
-        num2 = txtnum2.Text
-
-        If optSuma.Checked Then 'If -> si de condicion
-            lblrespuesta.Text = num1 + num2 'realizamos la suma
-        End If
-
-        If optResta.Checked Then
-            lblrespuesta.Text = num1 - num2 'realizamos la resta
-        End If
-
-        If optMultiplicar.Checked Then
-            lblrespuesta.Text = num1 * num2 'realizamos la multiplicacion
-        End If
-
-        If optDividir.Checked Then
-            lblrespuesta.Text = num1 / num2 'realizamos la division
-        End If
-
-        If optPorcentaje.Checked Then
-            lblrespuesta.Text = num1 * num2 / 100 'calcula el porcentaje ingresado
-        End If
-
-        If optexponente.Checked Then
-            lblrespuesta.Text = num1 ^ num2 'lo eleva al exponente que deseamos
-        End If
-
-        If optResiduo.Checked Then
-            lblrespuesta.Text = num1 Mod num2 'divison entera que da el residuo
-        End If
 
     End Sub
-
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
+    Private Sub btnProcesar_Click(sender As Object, e As EventArgs) Handles btnProcesar.Click
+        Dim ntabla, i As SByte
+        ntabla = txtntabla.Text
+        LstTablas.Items.Clear()
+        If ntabla <= 12 Then
+            For i = 1 To 10
+                LstTablas.Items.Add(ntabla.ToString() + "x" + i.ToString() + "=" + (ntabla * i).ToString())
+            Next
+        Else
+            LstTablas.Items.Add("Por favor ingrese solo tablas menores o igual a 12.")
+        End If
     End Sub
 End Class

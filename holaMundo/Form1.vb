506d0a52084799056dc1458e1f04ab0e1a31f5a9
Denys Alexander Hernandez Maravilla <usis016320@ugb.edu.sv>
2020-08-13 15:30:27 -0600

    se le quito el limite de la tabla hasta del 12 y se le agrego el ciclo Do While

--- holaMundo/Form1.vb
+++ holaMundo/Form1.vb
@@ -1,18 +1,14 @@
 ﻿Public Class Form1
-    Private Sub btnCalcular_Click(sender As Object, e As EventArgs)
-
-
-    End Sub
     Private Sub btnProcesar_Click(sender As Object, e As EventArgs) Handles btnProcesar.Click
-        Dim ntabla, i As SByte
+        Dim ntabla, i As Int64
         ntabla = txtntabla.Text
+        i = 1
+
         LstTablas.Items.Clear()
-        If ntabla <= 12 Then
-            For i = 1 To 10
-                LstTablas.Items.Add(ntabla.ToString() + "x" + i.ToString() + "=" + (ntabla * i).ToString())
-            Next
-        Else
-            LstTablas.Items.Add("Por favor ingrese solo tablas menores o igual a 12.")
-        End If
+        Do While i <= 10
+            LstTablas.Items.Add(ntabla.ToString() + "x" + i.ToString() + "=" + (ntabla * i).ToString())
+            i = i + 1
+        Loop
+
     End Sub
 End Class
